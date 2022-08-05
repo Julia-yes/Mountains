2b10b7e8f53e5a9628dc8e5de12f7460938e0dcd
Julia-yes <ec.ho@mail.ru>
2022-08-05 15:14:43 +0400

    reduced images

--- js/script.js
+++ js/script.js
@@ -1,22 +1,26 @@
-const hamburger = document.querySelector(".hamburger");
-const nav = document.querySelector(".h-list");
+document.addEventListener("DOMContentLoaded", function(event) { 
+  const hamburger = document.querySelector(".hamburger");
+  const nav = document.querySelector(".h-list");
 
-hamburger.addEventListener('click', function() {
-  hamburger.classList.toggle("hamburger_close");
-  nav.classList.toggle("h-list_phone");
-  body.classList.toggle("modal-window");
+  hamburger.addEventListener('click', function() {
+    hamburger.classList.toggle("hamburger_close");
+    nav.classList.toggle("h-list_phone");
+    body.classList.toggle("modal-window");
+  });
+
+  let windiwScreen = () => {
+    console.log (window.screen.width)
+    if (window.screen.width > 768) {var myFullpage = new fullpage('#fullpage', {           
+        navigation: true,
+        })}
+    else {
+      fullpage_api.destroy();
+    }
+  }
+
+  window.addEventListener('resize', () => {windiwScreen()})
+
 });
 
-let windiwScreen = () => {
-  console.log (window.screen.width)
-  if (window.screen.width > 768) {var myFullpage = new fullpage('#fullpage', {           
-      navigation: true,
-      })}
-  else {
-    fullpage_api.destroy();
-  }
-}
 
-
-window.addEventListener('resize', () => {windiwScreen()})
 
