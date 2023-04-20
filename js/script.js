b3d16fe45b2476fb61e9fd8da7ad3f2cd4e2233b
Julia-yes <ec.ho@mail.ru>
2023-04-20 16:22:54 +0400

    refactor: fix bug

--- js/script.js
+++ js/script.js
@@ -1,22 +1,9 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
+document.addEventListener("DOMContentLoaded", function (event) {
   const hamburger = document.querySelector(".hamburger");
   const nav = document.querySelector(".h-list");
 
-  hamburger.addEventListener('click', function() {
+  hamburger.addEventListener("click", function () {
     hamburger.classList.toggle("hamburger_close");
     nav.classList.toggle("h-list_phone");
-    body.classList.toggle("modal-window");
   });
-
-  
-
-  // let windiwScreen = () => {
-  //   if (window.screen.width < 768) {$.scrollify.disable()}
-  // };
-
-  // window.addEventListener('resize', () => {windiwScreen()})
-
 });
-
-
-
